5a3f9a96544fb47a94f21fb9f20a61cbbc14b018
Arnav Gupta <dev@championswimmer.in>
2018-07-26 18:50:56 +0530

    Update Fluent.java

--- design_patterns/Fluent.java
+++ design_patterns/Fluent.java
@@ -1,4 +1,25 @@
 class Fluent {
-
   
+  class Vehicle {
+    int wheels;
+    String color;
+    boolean automatic;
+    
+    Vehicle setWheels(int wheels) {
+      this.wheels = wheels;
+      return this;
+    }
+    Vehicle setColor(String color) {
+      this.color = color;
+      return this;
+    }
+    Vehicle setAutomatic(boolean automatic) {
+      this.automatic = automatic;
+      return this;
+    }
+  }
+  
+  public static void main (String[] argv) {
+    Vehicle v = new Vehicle().setWheels(4).setColor("green").setAutomatic(false);
+  }
 }
